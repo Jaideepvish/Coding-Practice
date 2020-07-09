d42f84e994959409d027c69d47ef7f5c1ad31307
jaideepvish <jaideepvish@localhost>
2020-07-08 21:32:19 -0400

    Initial Commit

---
+++ src/com/jaiworld/exercise/FirstProgram.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.jaiworld.exercise;
+
+/**
+ * @author jaideepvish
+ *
+ */
+public class FirstProgram {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Initial commit");
+
+	}
+
+}
